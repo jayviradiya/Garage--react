e8b952f3fef4a5280cd3de18ab8405c0689e45bf
jay <jayviradiya58885@gmail.com>
2024-04-06 20:55:44 +0530

    change

--- src/Garage-API/Header.js
+++ src/Garage-API/Header.js
@@ -8,6 +8,9 @@ import { GrTechnology } from "react-icons/gr";
 import { RiCustomerService2Fill } from "react-icons/ri";
 import { IoHomeOutline } from "react-icons/io5";
 
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
@@ -20,7 +23,8 @@ import { FaGooglePlusG } from "react-icons/fa";
 const Header = () => {
   return (
     <div>
-      <Container fluid className='ps-sticky border-bottom shadow'>
+
+      {/* <Container fluid className='ps-sticky border-bottom shadow'>
         <Container className='pt-1  pos-sticky pb-3'>
           <Row className='align-items-center'>
             <div className='col-3 p-0'>
@@ -45,7 +49,42 @@ const Header = () => {
             </div>
           </Row>
         </Container>
-      </Container>
+      </Container> */}
+
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid className='ps-sticky py-2 border-bottom shadow'>
+          <Container>
+            <Row className='d-flex justify-content-between py-1'>
+              <div className='col-2'>
+                <Navbar.Brand href="/"><img src="https://autocare.dexignlab.com/xhtml/images/logo-black.png" width={200} className='pt-2' alt="" /></Navbar.Brand>
+              </div>
+              <div className='col-6 d-flexx justify-content-end'>
+                <Navbar.Toggle aria-controls="basic-navbar-nav " className='' />
+                <Navbar.Collapse id="basic-navbar-nav" className='d-flex justify-content-end'>
+                  <Nav className="d-flex top justify-content-center">
+                    <Nav.Link as={Link} to="/" className='text-black ps-5 fs-5 hov'>Home</Nav.Link>
+                    <Nav.Link as={Link} to="/about-us" className='text-black ps-5 fs-5 hov'>About</Nav.Link>
+                    <Nav.Link as={Link} to="/all/service" className='text-black ps-5 fs-5 hov'>Service</Nav.Link>
+                    <Nav.Link as={Link} to="/technician-login" className='text-black ps-5 fs-5 hov'>Technician</Nav.Link>
+                    <Nav.Link as={Link} to="/customer-login" className='text-black ps-5 fs-5 hov'>Customer</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </div>
+              <div className="col-4 d-flex d-none d-lg-block">
+                <Navbar.Collapse id="basic-navbar-nav" className='d-end'>
+                  <Nav className="d-flex mt-2 justify-content-end">
+                    <Nav.Link as={Link} to="/" className='text-black ps-4 fs-5 hov'><FaInstagram /></Nav.Link>
+                    <Nav.Link as={Link} to="/about-us" className='text-black ps-4 fs-5 hov'><FaFacebookF /></Nav.Link>
+                    <Nav.Link as={Link} to="/all/service" className='text-black ps-4 fs-5 hov'><FaTwitter /></Nav.Link>
+                    <Nav.Link as={Link} to="/technician-login" className='text-black ps-4 fs-5 hov'><FaGooglePlusG /></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+              </div>
+            </Row>
+          </Container>
+        </Container>
+      </Navbar>
+
     </div>
   )
 }
